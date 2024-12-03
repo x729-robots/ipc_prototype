f1d8083c0cece15413fafe88ef45b43f1f787b15
x729-robots <x729-robots@yandex.ru>
2024-12-04 00:35:38 +0300

    /*/ испоравил seg_fault - задвоились переменные... и заработала сигнальная переменная

--- leading.cpp
+++ leading.cpp
@@ -1,20 +1,23 @@
 #include "leading.h"
 
+
 int main(int argc, char* argv[])
 {
-
+    int i;
+    i++;
+    
     pthread_mutex_t* pmutex = NULL;
     pthread_mutexattr_t attrmutex;
 
-    /* Initialise attribute to mutex. */
     pthread_mutexattr_init(&attrmutex);
+
+    /* Initialise attribute to mutex. */
+
     pthread_mutexattr_setpshared(&attrmutex,
-                                 PTHREAD_PROCESS_SHARED); // используем базовую реализацию мьютекса, а не от std::, так как в std::mutex не
-                                                          // поддерживается PTHREAD_PROCESS_SHARED, а это важно для моей мультисервисной архитектуры
+                                PTHREAD_PROCESS_SHARED); // используем базовую реализацию мьютекса, а не от std::, так как в std::mutex не
+    //  поддерживается PTHREAD_PROCESS_SHARED, а это важно для моей мультисервисной архитектуры
     // TODO переделать на cactus_rt mutex, так как там всё это продумано
 
-    pthread_cond_t* condition;
-    pthread_mutex_t* mutex;
     char* message;
     int des_cond, des_msg, des_mutex;
     int mode = S_IRWXU | S_IRWXG;
@@ -33,10 +36,10 @@ int main(int argc, char* argv[])
         exit(-1);
     }
 
-    mutex = (pthread_mutex_t*)mmap(NULL, sizeof(pthread_mutex_t), PROT_READ | PROT_WRITE, MAP_SHARED, des_mutex, 0);
+    pmutex = (pthread_mutex_t*)mmap(NULL, sizeof(pthread_mutex_t), PROT_READ | PROT_WRITE, MAP_SHARED, des_mutex, 0);
     /* Initialise mutex. */
 
-    if (mutex == MAP_FAILED)
+    if (pmutex == MAP_FAILED)
     {
         perror("Error on mmap on mutex\n");
         exit(1);
@@ -49,8 +52,9 @@ int main(int argc, char* argv[])
 
     /* Initialise attribute to condition. */
     pthread_condattr_init(&attrcond);
-    pthread_condattr_setpshared(&attrcond, PTHREAD_PROCESS_SHARED); // аналогично, std:: не поддерживает PTHREAD_PROCESS_SHARED, поэтому используем C
-    // TODO найти в boost  PTHREAD_PROCESS_SHARED для condition variable
+    pthread_condattr_setpshared(&attrcond, PTHREAD_PROCESS_SHARED); // аналогично, std:: не поддерживает PTHREAD_PROCESS_SHARED, поэтому используем
+    // C
+    //  TODO найти в boost  PTHREAD_PROCESS_SHARED для condition variable
 
     des_cond = shm_open(leading_cv, O_CREAT | O_RDWR | O_TRUNC, mode);
 
@@ -66,9 +70,9 @@ int main(int argc, char* argv[])
         exit(-1);
     }
 
-    condition = (pthread_cond_t*)mmap(NULL, sizeof(pthread_cond_t), PROT_READ | PROT_WRITE, MAP_SHARED, des_cond, 0);
+    pcond = (pthread_cond_t*)mmap(NULL, sizeof(pthread_cond_t), PROT_READ | PROT_WRITE, MAP_SHARED, des_cond, 0);
 
-    if (condition == MAP_FAILED)
+    if (pcond == MAP_FAILED)
     {
         perror("Error on mmap on condition\n");
         exit(1);
@@ -81,13 +85,13 @@ int main(int argc, char* argv[])
     {
         sleep(1);
 
-        pthread_mutex_lock(mutex);
-        pthread_cond_signal(condition);
+        pthread_mutex_lock(pmutex);
+        pthread_cond_signal(pcond);
         printf("son signaled\n");
-        pthread_mutex_unlock(mutex);
+        pthread_mutex_unlock(pmutex);
     }
 
-    //TODO подумать, что где забыл, привести в более красивый вид
+    // TODO подумать, что где забыл, привести в более красивый вид
 
     // освобождаем ресурсы
     pthread_mutex_destroy(pmutex);
